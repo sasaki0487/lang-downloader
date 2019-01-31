61ef0b2c22911f448f66b1c0cbcea9c09cb3b784
sasaki5221 <bobbytsai040@gmail.com>
2019-02-01 03:14:54 +0800

    first version

--- test.py
+++ test.py
@@ -1,28 +1,51 @@
 # coding=UTF-8
-import os
+import os.path
 import ffmpy
 import urllib3
 import re
 import sys
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 http = urllib3.PoolManager()
-addr = raw_input('請輸入直播網址：')
+addr = input('請輸入直播網址：')
 r = http.request('GET', addr)
 #https://web.langlive.com/html/share/template2.html?live_id=3669719Y49409glOL
 print(r.status)
 if r.status != 200 :
     print('獲取網址失敗1')
     sys.exit()
-addr = re.search(r'https:\\\/\\\/playback(.*)m3u8',r.data)
+start = r.data.decode('utf-8').find('nickname\":\"')
+end = r.data.decode('utf-8').find('\"',start+11)
+nickname = r.data.decode('utf-8')[start+11:end]
+
+start = r.data.decode('utf-8').find('title\":\"')
+end = r.data.decode('utf-8').find('\"',start+8)
+title = r.data.decode('utf-8')[start+8:end]
+
+addr = re.search(r'https:\\\/\\\/playback(.*)m3u8',r.data.decode('utf-8'))
 if not addr :
     print('獲取網址失敗2')
     sys.exit()
 addr = re.sub(r'\\','',addr.group())
+
+start = addr.find('--')
+end = addr.find('.m3u8')
+date = addr[start+2:end]
 print(addr)
+out = date + '.mp4'
+name = nickname + '_' + title + '_' + date + '.mp4'
+name = re.sub(r'[\\\/:\*\?<>|]','',name)
+print(name) 
 
 ff = ffmpy.FFmpeg(
     inputs = { addr : None},
-    outputs = {'out.mp4' : '-c copy'}
+    outputs = { out : '-c copy'}
 )
 
 ff.run()
+
+if os.path.isfile(out):
+	os.rename(out, name)
+else :
+	print('轉換錯誤')
+	sys.exit()
